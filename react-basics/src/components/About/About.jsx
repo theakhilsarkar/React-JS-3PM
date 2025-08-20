01cc1b585fbf7cade6fc7837c1ba01cc97c2246d
theakhilsarkar <akhilsodvadiya7@gmail.com>
2025-08-20 16:05:33 +0530

    20/08/2025

--- react-basics/src/components/About/About.jsx
+++ react-basics/src/components/About/About.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import "./About.css";
 
-export default function About() {
+export default function About(props) {
+  const { name, bio, avatar_url, followers, following, company, location } =
+    props.user;
   return (
     <section className="about-section">
       <div className="detail">
-        <h1>Hello, I'm Jugal Rana</h1>
+        <h1>Hello, I'm {name}</h1>
         <h3>MERN Stack Developer</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nemo quos
-          id adipisci voluptate aliquid repudiandae ea magni dolor et! Lorem
-          ipsum dolor sit amet consectetur adipisicing elit. Ea, nesciunt.
-        </p>
+        <p>{bio}</p>
+        <p>{company}</p>
+        <p>{location}</p>
+        <div className="github-stats">
+          <p className="follower">
+            <span>Follower - </span>
+            <span> {followers}</span>
+          </p>
+          <p className="follower">
+            <span>Following - </span>
+            <span> {following}</span>
+          </p>
+        </div>
         <button className="hire-btn">Hire Me</button>
         <button className="resume-btn">Resume</button>
       </div>
+
+      <img src={avatar_url} alt="" />
     </section>
   );
 }
+
+
+// prop drilling - 
--- react-basics/src/screens/Home/Home.jsx
+++ react-basics/src/screens/Home/Home.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import About from "../../components/About/About";
 import Projects from "../../components/Projects/Projects";
+import axios from "axios";
 
 export default function Home() {
+  const [githubData, setGithubData] = useState({});
+  // to call api one time
+  useEffect(() => {
+    fetchApi().then((data) => setGithubData(data));
+  }, []);
+
+  const fetchApi = async () => {
+    const res = await axios.get("https://api.github.com/users/theakhilsarkar");
+    return res.data;
+  };
+
   return (
     <div>
-      <About />
+      <About user={githubData} />
       <Projects />
     </div>
   );
 }
+
+// Home - Data
+// About
+// Detail
+// Follower - count
