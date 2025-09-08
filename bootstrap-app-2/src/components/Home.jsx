e631cf8489aac8f53c7091da72ddb2a4d338b638
theakhilsarkar <akhilsodvadiya7@gmail.com>
2025-09-08 16:01:23 +0530

    08/09/2025

---
+++ bootstrap-app-2/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hero from './Hero'
+import NewsFeed from './NewsFeed'
+
+export default function Home() {
+    return (
+        <div>
+            <Hero />
+            <NewsFeed />
+        </div>
+    )
+}
--- bootstrap-app-2/src/main.jsx
+++ bootstrap-app-2/src/main.jsx
@@ -5,12 +5,18 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 //dist/js/bootstrap.bundle.min.js
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { BrowserRouter } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
 
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 
 )
+
+// 1. wrap app into browser router
+// 2. define routes --> pages/components, route --> component,page
 
 
 // layout bootstrap 
